290c08015261c1de9d3da61e19588d4f450b1d80
E.S <extrin13@gmail.com>
2019-04-02 23:22:21 +0300

    chore: Create webpack config and empty entry point

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
